8bc5b6bec586f009e048718b50bb4d3d899deef2
Jake1427 <jakedl28@g.eips.ca>
2026-02-26 10:15:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let ball = {
   x: canvas.width/2,
   y: canvas.height/2,
   radius: 10,
-  speedX: 2,
-  speedY: 2
+  speedX: 4,
+  speedY: 4
 };
 
 let paddleWidth = 10;
@@ -97,3 +97,4 @@ function gameLoop() {
 
 
 setInterval(gameLoop, 1000/60);
+
